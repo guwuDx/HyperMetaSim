61e79806d77c0ef22e0d86340f0f0a53e69b6beb
guwuDx <121189054+guwuDx@users.noreply.github.com>
2025-01-30 23:23:45 +0800

    ...

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from cst.interface import DesignEnvironment
 def main():
     # cst_app = misc.cst_conn()
     # print(csti.DesignEnvironment)
-    de = DesignEnvironment.new()
-    
-    de.close()
+    de = DesignEnvironment.connect_to_any_or_new()
+    crr_proj = de.get_open_projects()
+    print(crr_proj[0], '\n---')
+    # de.close()
 
 
 if __name__ == '__main__':
--- utils/misc.py
+++ utils/misc.py
@@ -13,12 +13,3 @@ def add_cst_lib_path():
     lib_path = read_toml("./config/service.toml", "cst")["install_path"]
     sys.path.append(lib_path)
 
-
-def cst_conn(name="CST.Application"):
-    cst_app = win32.Dispatch(name)
-    print("CST version: ", cst_app.GetVersion())
-    return cst_app
-
-
-def new_project(cst_app):
-    return cst_app.CreateNewProject()
